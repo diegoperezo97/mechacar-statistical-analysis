cad417ae78bd78d83700f9d21b2cb7c2e984fdc1
Diego Pérez Ortega <65054637+diegoperezo97@users.noreply.github.com>
2022-05-13 21:36:53 -0500

    Challenge 15 finished.

--- Wednesday, April 6th 2022/15-2-Student_Resources/05-Stu_ANOVA/Unsolved/hair_anova_starter.R
+++ Wednesday, April 6th 2022/15-2-Student_Resources/05-Stu_ANOVA/Unsolved/hair_anova_starter.R
@@ -1,6 +1,11 @@
+install.packages("ggplot2")
+library(ggplot2)
+
 # Read in the CSV file. 
-hair <- read.csv(file="../Resources/hair.csv")
+hair <- read.csv(file="/Users/diegoperezo97/Documents/Tecnológico de Monterrey – Data Analytics Boot Camp/mechacar-statistical-analysis/Wednesday, April 6th 2022/15-2-Student_Resources/05-Stu_ANOVA/Resources/hair.csv")
 
 #  Plot the data using ggplot
+ggplot(hair, aes(x=HairColour, y=Pain)) + geom_boxplot()
 
 # Determine the p-value using ANOVA
+summary(aov(Pain ~ HairColour, data=hair))
